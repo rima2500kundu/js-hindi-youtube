9df0074c0d2b9231d6610b4996f88d6980869223
Rima Kundu <rimasdgworks@gmail.com>
2025-07-09 11:16:27 +0000

    03 done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,65 @@
+/*************************** conversion *************************/
+let score = "rima"
+
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedin = "rima"
+
+let booleanIsLoogein = Boolean(isLoggedin)
+// console.log(booleanIsLoogein)
+
+// 1 => ture; 0 => false
+// "" => false; "rima" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+/*************************** operations *************************/
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Rima"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+// console.log(-true)
+
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+// console.log(gameCounter)
+
+let x = 3
+let y = x++
+
+console.log(`value of x is ${x} and value of y is ${y}`)
+
+let a = 3
+let b = ++a
+
+console.log(`value of a is ${a} and value of b is ${b}`)
+
+
