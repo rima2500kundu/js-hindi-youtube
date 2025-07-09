1723e65ff511bd53bf699959e82c8dff34e3ddd5
Rima Kundu <rimasdgworks@gmail.com>
2025-07-09 09:19:22 +0000

    added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+console.log(3+3)
+console.log("Rima")
+
+
+let name = "Rima"
+let age = 18
+let isLoggedin = false
+let state;
+let state2 = null
+
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalon value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null)
+console.log(typeof undefined)
