217241d87709fbe8265aab6e97b671391c4a84d5
Rima Kundu <rimasdgworks@gmail.com>
2025-07-09 13:04:52 +0000

    dataypes done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,46 @@
+// primitive
+// 7 types: string, number, boolean, null, undefined, symbol, bigint
+
+// js is dynamically typed language
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedin = false
+const outsideTemp = null
+
+let userEmail
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+// console.log(id === anotherId)
+
+const bigNumber = 454874543543556456n
+// console.log(typeof(bigNumber))
+
+// reference(non primitive)
+// array, object, function
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "Rima",
+    age: 18
+}
+
+const myFunc = function(){
+    console.log("hello world");
+    
+}
+// myFunc()
+
+
+console.log(typeof(heros));
+
+
+
+
+
+
+
+
