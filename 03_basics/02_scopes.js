e0c67ff6257af9721dd0643a60e5c3819082990d
Rima Kundu <rimasdgworks@gmail.com>
2025-07-22 09:09:12 +0000

    scope 2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,16 +6,45 @@ let a = 300
 if(true){
     let a  = 10
     const b = 20
-    console.log("inner:", a);
+    // console.log("inner:", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one(){
+    let username = "rima"
 
+    function two(){
+        let website = "youtube"
+        console.log(username);       
+    }
+    // console.log(website);  
+    // two()
+}
+// one()
 
+if(true){
+    const username = "rima"
 
+    if(username === "rima"){
+        const website = " youtube"
+        // console.log(username + website);        
+    }
+    // console.log(website);   
+}
+// console.log(username);
 
+// ++++++++++++++++++++ interesting +++++++++++++++++++
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
 
 
